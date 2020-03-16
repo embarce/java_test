ada584d071ae5e69c99aee2fc0d374afc8985aed
Embrace <729374717@qq.com>
2020-03-16 16:06:28 +0800

    Default Changelist

--- TestGit/TestGit.java
+++ TestGit/TestGit.java
@@ -9,5 +9,45 @@ public class TestGit {
     private  String QWQ;
     private  String OMO;
     private String yyy;
+
+    public String getTest() {
+        return test;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
+    }
+
+    public String getQAQ() {
+        return QAQ;
+    }
+
+    public void setQAQ(String QAQ) {
+        this.QAQ = QAQ;
+    }
+
+    public String getQWQ() {
+        return QWQ;
+    }
+
+    public void setQWQ(String QWQ) {
+        this.QWQ = QWQ;
+    }
+
+    public String getOMO() {
+        return OMO;
+    }
+
+    public void setOMO(String OMO) {
+        this.OMO = OMO;
+    }
+
+    public String getYyy() {
+        return yyy;
+    }
+
+    public void setYyy(String yyy) {
+        this.yyy = yyy;
+    }
 }
 
